3d75901770a4fa186d9df752fb00fd714bf7cfd8
Happy Misfit <happymisfit@pm.me>
2024-11-04 23:26:33 +0000

    learning about input function

---
+++ iptaker02.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+"""Alta3 Research | VRHaugen
+   print() - display data to std out"""
+
+# below is a function containing our code
+def main():
+    user_input = input("Please enter an IPv4 address: ")
+
+    print("You told me the IPv4 address is: ", user_input)
+
+main()
+
