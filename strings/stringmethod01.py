9970d4cf60cb184a0de141dc688fba5c42fc4316
Happy Misfit <happymisfit@pm.me>
2024-11-05 18:08:50 +0000

    Join and split

---
+++ strings/stringmethod01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#create a small string
+lilstring = "Alta3 Research offers classes on Python coding"
+newlist = lilstring.split(" ")
+print(newlist)
+
+myiplist = ["192","168","0","12"]
+singleip = ".".join(myiplist)
+print(singleip)
---
+++ strings/stringmethod02.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Alta3 Research || Author: Me"""
+
+def main():
+    """ Runtime code """
+    lilstring = "Alta3 Research offers classes on Python coding"
+    newlist = lilstring.split(" ")
+    print(newlist)
+
+    myiplist = ["192","168","0","12"]
+    singleip = ".".join(myiplist)
+    print(singleip)
+
+main()
